d57d16f45ee04339109a9c7bb5311e1aeda0adaa
Lucille Njoo <lucillenjoo@gmail.com>
2020-04-26 16:03:42 -0700

    Initial Commit

---
+++ Homework 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework 3
+//
+//  Created by Lucille Le Njoob on 4/26/20.
+//  Copyright © 2020 Lucille Njoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
